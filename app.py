9f24a3dba1ab3d85c96910422e440893312d408a
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-04 10:29:10 +0900

    Python Imports

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 # Real Estate Application in Python
+import os 
+import json 
+import time 
+from functools import cache 
+
+import requests 
+import numpy as np 
+import pandas as pd 
+
+import folium
+from folium.plugins import MarkerCluster
+from folium import MacroElement
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
