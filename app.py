e0f8dc3135959d16ae36840f0f13e3f44a85e440
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-04 10:58:28 +0900

    Adding marker color logic for real estate map

--- app.py
+++ app.py
@@ -33,4 +33,25 @@ def index():
 
         data_frame["annual_rent"] = data_frame["rentZestimate"] * 12
         data_frame["gross_rental_yield"] = (data_frame["annual_rent"] / data_frame["zestimate"]) * 100
-        
+
+        data_frame["gross_rental_yield"] = data_frame["gross_rental_yield"].replace([np.inf, -np.inf], np.nan)
+
+        def get_marker_color(gross_yield, off_market):
+            if off_market:
+                return "black"
+            elif pd.isna(gross_yield):
+                return "gray"
+            elif gross_yield < 5:
+                return "red"
+            elif gross_yield < 8:
+                return "orange"
+            else:
+                return "green"
+            
+        map_center = [data_frame["latitude"].mean(), data_frame["longitude"].mean()]
+
+        map = folium.Map(location=map_center, zoom_start=12)
+
+        marker_cluster = MarkerCluster().add_to(map)
+        for idx, row in data_frame.iterrows():
+            pass
